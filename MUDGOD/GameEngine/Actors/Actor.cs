1f90f98682a166e9b220037cebdac018d3abca90
AlexGowans <alexgowans@hotmail.co.uk>
2018-10-26 00:22:54 -0700

    Last touches on class and player init

--- MUDGOD/GameEngine/Actors/Actor.cs
+++ MUDGOD/GameEngine/Actors/Actor.cs
@@ -23,6 +23,22 @@ namespace MUDGOD{
         public int defPoints;
 
         public Vector2 mapLocation = new Vector2(0,0);
-        //Initialise in main child
+
+        //INITIALISE
+        public Actor(string name = "No-name",
+                                int size = 1, int hp = 100, int mp = 100, int str = 10, int dex = 10, int intP = 10, int lck = 10, int def = 10,
+                                int locX = 0, int locY = 0) {
+            this.name = name;
+            this.bodySize = size;
+            this.healthPoints = this.healthPointsMax = hp;
+            this.manaPoints = this.manaPointsMax = mp;
+            this.strPoints = str;
+            this.dexPoints = dex;
+            this.intPoints = intP;
+            this.lckPoints = lck;
+            this.defPoints = def;
+            this.mapLocation.X = locX;
+            this.mapLocation.Y = locY;
+        }
     }
 }
--- MUDGOD/GameEngine/PlayerClasses/PlayerClass.cs
+++ MUDGOD/GameEngine/PlayerClasses/PlayerClass.cs
@@ -5,6 +5,7 @@ using System.Text;
 namespace MUDGOD {
     class PlayerClass {
         public string name;
+        public string description;
         public float hpMulti;
         public float mpMulti;
         public float strMulti;
@@ -12,22 +13,32 @@ namespace MUDGOD {
         public float intMulti;
         public float lckMulti;
         public float defMulti;
-        //Set in Defined Class Scripts
-        
-    }
 
-
-    class PeasantClass : PlayerClass {
-
-        public PeasantClass(string name = "No Class", float hp = 1, float mp = 1, float str = 1, float dex = 1, float intP = 1, float lck = 1, float def = 1) {
+        public PlayerClass(string name = "No Class", string desc = "No description", float hp = 1, float mp = 1, float str = 1, float dex = 1, float intP = 1, float lck = 1, float def = 1) {
             this.name = name;
-            this.hpMulti = hp;
-            this.mpMulti = mp;
+            this.hpMulti  = hp;
+            this.mpMulti  = mp;
             this.strMulti = str;
             this.dexMulti = dex;
             this.intMulti = intP;
             this.lckMulti = lck;
             this.defMulti = def;
+        }
+
+    }
+
+
+    class PeasantClass : PlayerClass {
+        public PeasantClass() {
+            this.name = "Peasant";
+            this.description = "You are a peasant";
+            this.hpMulti  = 0.3f;
+            this.mpMulti  = 0.3f;
+            this.strMulti = 0.3f;
+            this.dexMulti = 0.3f;
+            this.intMulti = 0.3f;
+            this.lckMulti = 0.3f;
+            this.defMulti = 0.3f;
         }
     }
 }
