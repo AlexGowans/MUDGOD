ec8febb608feeb29d4eff910230c0f0ae62f13b5
Alex Gowans <alexgowans@hotmail.co.uk>
2018-10-26 19:37:16 +0100

    Delete TOKEN.cs

--- MUDGOD/Core/Data/TOKEN.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace MUDGOD {
-    class TOKEN {
-        public string token = "NTA0MzgzNDQzODk1OTc1OTM2.DrTTtA.jv7Tw2PoJk-4vlp1ApIG8J1V8c8";
-    }
-}
