80e14ea7cd46b64708cd53c3f5d75f12508f04eb
AlexGowans <alexgowans@hotmail.co.uk>
2018-10-25 22:54:43 -0700

    Embed tutorial

--- MUDGOD/Core/Commands/cmdHelloWorld.cs
+++ MUDGOD/Core/Commands/cmdHelloWorld.cs
@@ -1,17 +1,41 @@
-﻿using System;
+﻿/*
+ This Script is an an example of how to use command features
+ 
+ 
+ 
+ */
+
+using System;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using System.Text;
 
-
 using Discord;
 using Discord.Commands;
 
+
+
 namespace MUDGOD.Core.Commands {
     public class cmdHelloWorld : ModuleBase<SocketCommandContext> {
+
+        //HELLO
         [Command("hello"), AliasAttribute("helloworld", "world"), Summary("Hello World command")]
-        public async Task Sjustin() {
+        public async Task Hello() {
             await Context.Channel.SendMessageAsync("Hello World");
+        }
+
+        //EMBED
+        [Command("embed"), Summary("Embedding example")]
+        public async Task Embed([Remainder]string input = "None") {
+            EmbedBuilder Embed = new EmbedBuilder();
+            Embed.WithAuthor("MUD GOD", Context.User.GetAvatarUrl());
+            Embed.WithColor(40, 200, 150);
+            Embed.WithFooter("Embed Footer", Context.Guild.Owner.GetAvatarUrl());
+            Embed.WithDescription("This is a shitty description\n" + "[This is a link](https://www.google.com/)");
+            Embed.AddInlineField("User input:", input);
+
+
+            await Context.Channel.SendMessageAsync("", false, Embed.Build());
         }
     }
 }
